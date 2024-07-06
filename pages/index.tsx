8bbfd471635ffe68dded2940e84cfcbbc1a2f042
techygrrrl <techygrrrl@proton.me>
2024-07-06 18:45:13 -0400

    adjust branding

--- pages/index.tsx
+++ pages/index.tsx
@@ -40,7 +40,7 @@ export default function Home() {
       <main
         className={`flex min-h-screen flex-col items-center p-24 ${inter.className}`}
       >
-        <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full  before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic  after:blur-2xl after:content-[''] before:bg-gradient-to-br before:from-transparent before:to-pink-700/10 after:from-pink-900 after:via-[#EF15BF]/40 before:lg:h-[360px]">
+        <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full  before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic  after:blur-2xl after:content-[''] before:bg-gradient-to-br before:from-transparent before:to-[#EF15BF]/10 after:from-[#EF15BF]/30 after:via-[#EF15BF]/40 before:lg:h-[360px]">
           <Image
             className="relative drop-shadow-[0_0_0.3rem_#ffffff70]"
             src="/queuerrr-logo.svg"
